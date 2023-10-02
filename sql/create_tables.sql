338cca3080634b141b7511b8c4bf91f519cb2437
rosa <chatzigeorgiour@interamerican.gr>
2023-10-02 15:55:20 +0300

    dockerize db

---
+++ sql/create_tables.sql
@@ -0,0 +1,39 @@
+CREATE TABLE accounts (
+    id UUID PRIMARY KEY,
+    balance DECIMAL(19, 4),
+    currency VARCHAR(255),
+    createdAt TIMESTAMP,
+    version INT
+);
+
+CREATE TABLE transactions (
+    id UUID PRIMARY KEY,
+    source_account_id UUID,
+    target_account_id UUID,
+    amount DECIMAL(19, 4),
+    currency VARCHAR(255)
+);
+
+ALTER TABLE transactions
+ADD CONSTRAINT FK_source_account
+FOREIGN KEY (source_account_id)
+REFERENCES accounts(id);
+
+ALTER TABLE transactions
+ADD CONSTRAINT FK_target_account
+FOREIGN KEY (target_account_id)
+REFERENCES accounts(id);
+
+
+CREATE TABLE transaction_requests (
+    requestId UUID PRIMARY KEY,
+    transaction_id UUID,
+    requestStatus VARCHAR(255),
+    jsonBody TEXT,
+    errorMessage TEXT
+);
+
+ALTER TABLE transaction_requests
+ADD CONSTRAINT FK_transaction
+FOREIGN KEY (transaction_id)
+REFERENCES transactions(id);
